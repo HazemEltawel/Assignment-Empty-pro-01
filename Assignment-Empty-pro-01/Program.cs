7c965d1cf2c6ec10b7591e09d250ce5cb0b0ba85
HazemEltawel <eltawelhazem5@gmail.com>
2025-09-05 22:44:57 +0300

    Add project files.

---
+++ Assignment-Empty-pro-01/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Assignment_Empty_pro_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CreateHostBuilder(args).Build().Run();
+            var webapplicationBuilber = WebApplication.CreateBuilder();
+            webapplicationBuilber.Services.AddControllersWithViews();
+
+            var app = webapplicationBuilber.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+            app.MapGet("/hamada", async context =>
+            {
+                await context.Response.WriteAsync("Hello hamada!");
+            });
+            app.Run();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+        }
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
+
+
+
+    }
+}
